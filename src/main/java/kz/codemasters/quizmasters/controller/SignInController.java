a9e3dfe2b4ecf480761d7fe876e4439c94524bef
serikzhilibayev <serik.zhilibayev@nu.edu.kz>
2017-07-20 15:05:02 +0600

    Redirection of Page if person is already registered

--- src/main/java/kz/codemasters/quizmasters/controller/SignInController.java
+++ src/main/java/kz/codemasters/quizmasters/controller/SignInController.java
@@ -5,13 +5,16 @@ import kz.codemasters.quizmasters.StringsValidator;
 import kz.codemasters.quizmasters.model.User;
 import kz.codemasters.quizmasters.repository.interfaces.UserRepository;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * Created by aidar on 7/16/17.
@@ -22,27 +25,37 @@ public class SignInController {
 
     private String email;
     private String password;
+
     @Inject
     StringsValidator stringsValidator;
     @EJB
     UserRepository userRepository;
 
+    @PostConstruct
+    public void init() {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        HttpSession httpSession = (HttpSession) context.getSession(true);
+        if (httpSession.getAttribute(AppConstant.SESSION_USER) != null){
+            try {
+                FacesContext.getCurrentInstance().getExternalContext().redirect(context.getRequestContextPath()+"/mainpage.xhtml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public boolean SignIn() {
         if (!stringsValidator.validateEmail(email, FacesContext.getCurrentInstance())) return false;
         User user = userRepository.getUserByEmail(email);
